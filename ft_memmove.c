a860ae8b05414201dfb8d664a925c81e47851ea4
jquintin <108005850+zequintino@users.noreply.github.com>
2022-10-25 17:39:48 +0100

    deleted comments at malloc statements

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jquintin <jquintin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jquintin <jquintin@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 21:00:40 by jquintin          #+#    #+#             */
-/*   Updated: 2022/10/25 17:05:23 by jquintin         ###   ########.fr       */
+/*   Updated: 2022/10/25 17:36:29 by jquintin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	char	*tmp;
 	size_t	i;
 
-	// tmp = (char *)malloc(sizeof(char) * len);
+	tmp = (char *)malloc(sizeof(char) * len);
 	if (!dst || !tmp)
 		return (NULL);
 	i = -1;
@@ -26,6 +26,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	i = -1;
 	while (++i < len)
 		*((char *)dst + i) = *(tmp + i);
-	// free(tmp);
+	free(tmp);
 	return (dst);
 }
